69c6015b796cfbb42024439efa9231c0065e75d1
Maxim Uchaev <o.otaku@list.ru>
2023-11-27 20:30:54 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="app">
+    <header class="app-header">
+      <div class="app-header__container _container">"Лого" "Навигация"</div>
+    </header>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
