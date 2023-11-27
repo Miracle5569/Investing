1b0ff852005894beebd0f3ba0cc5cbbcf945226f
Maxim Uchaev <o.otaku@list.ru>
2023-11-27 22:46:41 +0500

    header

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,83 @@
 <template>
   <div id="app">
     <header class="app-header">
-      <div class="app-header__container _container">"Лого" "Навигация"</div>
+      <div class="app-header__container _container">
+        <div class="app-header__logo">
+          <img
+            class="app-header__logo-image"
+            src="/src/assets/images/investing.com.png"
+            alt="logo"
+          />
+        </div>
+        <ul class="app-header__navigation">
+          <li class="app-header__navigation-items">
+            <font-awesome-icon
+              class="app-header__navigation-icon"
+              icon="fa-solid fa-hand-holding-dollar"
+            />Investing
+          </li>
+          <li class="app-header__navigation-items">
+            <font-awesome-icon
+              class="app-header__navigation-icon"
+              icon="fa-solid fa-book-open"
+            />Curse
+          </li>
+          <li class="app-header__navigation-items">
+            <font-awesome-icon class="app-header__navigation-icon" icon="fa-solid fa-question" />Who
+            are we
+          </li>
+          <li class="app-header__navigation-items">
+            <font-awesome-icon
+              class="app-header__navigation-icon"
+              icon="fa-solid fa-user-tie"
+            />Profile
+          </li>
+        </ul>
+      </div>
     </header>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.app-header {
+  &__container {
+    padding-top: 10px;
+    max-height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__logo {
+    position: relative;
+    width: 50px;
+    height: 50px;
+
+    &-image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 10px;
+    }
+  }
+  &__navigation {
+    display: flex;
+    justify-content: space-between;
+    width: 550px;
+
+    &-items {
+      font-size: 20px;
+      &:hover {
+        cursor: pointer;
+        &:not(:first-child) {
+          transform: rotate(-5deg);
+          margin-top: 5px;
+        }
+      }
+    }
+
+    &-icon {
+      margin-right: 10px;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import './assets/main.css'
+import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +6,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faHandHoldingDollar,
+  faBookOpen,
+  faQuestion,
+  faUserTie
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHandHoldingDollar, faBookOpen, faQuestion, faUserTie)
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
